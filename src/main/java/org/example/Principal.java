de2b396be77c32194ef45a94a6ca42fb65349800
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-03-08 17:37:38 -0300

    Pretty print

--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -29,7 +29,9 @@ public class Principal {
         funcionarios.removeIf(f -> f.nome.equals("João"));
 
         // 3.3 Imprimir todos os funcionários no formato correto
+        System.out.println("==> Imprimir todos os funcionários");
         funcionarios.forEach(System.out::println);
+        System.out.println();
 
         // 3.4 Aplicar um aumento de 10% ao salário de todos os funcionários
         funcionarios.forEach(f -> f.aplicarAumento(10));
@@ -41,7 +43,9 @@ public class Principal {
                 );
 
         // 3.6 Imprimir os funcionários agrupados por função
+        System.out.println("==> Funcionários por função");
         System.out.println(funcionariosPorFuncao);
+        System.out.println();
 
         // 3.7 Imprimir os funcionários que fazem aniversário no mês 10 e 12
         String funcionariosAniversario10ou12 = funcionarios.stream()
@@ -52,18 +56,19 @@ public class Principal {
                 .map(Funcionario::getNome)
                 .collect(Collectors.joining(", "));
 
-        System.out.println("Os funcionários que fazem aniversário no mes 10 ou 12 são: " + funcionariosAniversario10ou12);
+        System.out.println("==> Os funcionários que fazem aniversário no mes 10 ou 12 são: " + funcionariosAniversario10ou12);
+        System.out.println();
 
         // 3.8 Imprimir o funcionário com maior idade
         Optional<Funcionario> maisVelho = funcionarios.stream()
                 .reduce((f1, f2) -> f1.getDataNascimento().isBefore(f2.getDataNascimento()) ? f1 : f2);
 
         maisVelho.ifPresent(f -> {
-            System.out.println("Funcionário mais velho: " + f.getNome() + " com idade: " + Period.between(f.getDataNascimento(), LocalDate.now()).getYears());
+            System.out.println("==> Funcionário mais velho: " + f.getNome() + " (" + Period.between(f.getDataNascimento(), LocalDate.now()).getYears() + ")");
         });
+        System.out.println();
 
-
-        // 3.9 Imprimir a lista de funcionários por odem alfabética
+        // 3.9 Imprimir a lista de funcionários por ordem alfabética
         List<Funcionario> ordenado = funcionarios.stream()
                         .sorted(Comparator.comparing(Funcionario::getNome))
                         .toList();
@@ -71,39 +76,24 @@ public class Principal {
         String ordem = ordenado.stream()
                         .map(Funcionario::getNome)
                                 .collect(Collectors.joining(", "));
-        System.out.println("Nome dos funcionários em ordem alfabética");
+        System.out.println("==> Nome dos funcionários em ordem alfabética");
         System.out.println(ordem);
+        System.out.println();
 
+        // 3.10 Imprimir o total dos salários dos funcionários
         BigDecimal salarioTotal = funcionarios.stream()
                         .map(Funcionario::getSalario)
                         .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        System.out.println("A soma de todos os salários é R$ " + salarioTotal);
-
-        BigDecimal soma = BigDecimal.ZERO;
-        for (Funcionario f : funcionarios) {
-            soma = soma.add(f.getSalario());
-        }
-
-        System.out.println("Soma total dos salários: R$ " +soma);
-
-        final BigDecimal salario_min = BigDecimal.valueOf(1212.00);
-
-        Map<String, BigDecimal> funcionarioSalario = funcionarios.stream()
-                .collect(Collectors.toMap(
-                        Funcionario::getNome,
-                f -> f.getSalario().divide(salario_min, 2, RoundingMode.HALF_UP))
-                );
-
-        funcionarioSalario.forEach((nome, salariosMinimos) -> {
-            System.out.println(nome + " ganha " + salariosMinimos + " salários mínimos.");
-        });
+        System.out.println("==> A soma de todos os salários é R$ " + salarioTotal);
+        System.out.println();
+        // 3.11 Imprimir quantos salários mínimos ganha cada funcionário
 
         BigDecimal salarioMin = new BigDecimal("1212.00");
 
         for (Funcionario f : funcionarios) {
             BigDecimal qtdSalarioMin = f.getSalario().divide(salarioMin, 2, RoundingMode.HALF_DOWN);
-            System.out.println(f.getNome() + " ganha " + qtdSalarioMin + "salários mínimos");
+            System.out.println(f.getNome() + " ganha " + qtdSalarioMin + " salários mínimos");
         }
     }
 }
